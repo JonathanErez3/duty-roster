b13d806e6488ad6ba25c7afa11d4fbfde3ef5885
JonathanErez3 <jonathanerez92@gmail.com>
2026-04-25 19:51:09 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from “react”;
+import { useState, useEffect } from "react";
 
 const DAYS = [“ראשון”, “שני”, “שלישי”, “רביעי”, “חמישי”];
 const DUTY_TYPES = [“רסר”, “מטבח”];
